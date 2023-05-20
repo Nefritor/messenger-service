dd2b6fbc1a939666775ff86c6171bb312cc0da36
NEFRITOR-PC\Nefritor <nefritor@gmail.com>
2023-05-20 23:32:31 +0500

    v 0.3.2 - constructor application remove

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import fs from 'fs';
-
 import minimist from 'minimist';
 
 import {startApp as startMessengerApp} from './service/Messenger/Controller.js';
-import {startApp as startConstructorApp} from './service/Constructor/Controller.js';
 
 const {secured = false} = minimist(process.argv.splice(2));
 
